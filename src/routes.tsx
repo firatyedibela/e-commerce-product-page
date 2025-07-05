42560b608a7fda2797a267977b7cc4368f59a7d2
firatyedibela <yedibelafirat0@gmail.com>
2025-07-05 20:37:49 +0300

    feat: add routes

---
+++ src/routes.tsx
@@ -0,0 +1,29 @@
+type Route = {
+  name: string;
+  href: string;
+};
+
+export type Routes = Route[];
+
+export const routes: Route[] = [
+  {
+    name: 'collections',
+    href: '#',
+  },
+  {
+    name: 'men',
+    href: '#',
+  },
+  {
+    name: 'women',
+    href: '#',
+  },
+  {
+    name: 'about',
+    href: '#',
+  },
+  {
+    name: 'contact',
+    href: '#',
+  },
+];
