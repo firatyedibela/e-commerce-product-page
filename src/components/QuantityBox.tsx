2a52d41b2e9cacb5fa7e6dff856cb633605ee2ca
firatyedibela <yedibelafirat0@gmail.com>
2025-07-05 17:42:23 +0300

    feat: implement QuantityBox component

---
+++ src/components/QuantityBox.tsx
@@ -0,0 +1,16 @@
+import iconMinus from '../assets/images/icon-minus.svg';
+import iconPlus from '../assets/images/icon-plus.svg';
+
+export const QuantityBox = () => {
+  return (
+    <div className="bg-grey-50 rounded-[10px] flex justify-between items-center w-[157px] h-[56px]">
+      <button type="button" className="cursor-pointer px-2 py-2">
+        <img src={iconMinus} alt="Minus icon" />
+      </button>
+      <span className="text-preset-3 text-grey-950">0</span>
+      <button type="button" className="cursor-pointer px-2 py-2">
+        <img src={iconPlus} alt="Plus icon" />
+      </button>
+    </div>
+  );
+};
