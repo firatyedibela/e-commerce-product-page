dda3973949e979afcf7288db43e9d0b985340882
firatyedibela <yedibelafirat0@gmail.com>
2025-07-06 17:41:55 +0300

    feat: remove hardcoded width and height

--- src/components/QuantityBox.tsx
+++ src/components/QuantityBox.tsx
@@ -3,7 +3,7 @@ import iconPlus from '../assets/images/icon-plus.svg';
 
 export const QuantityBox = () => {
   return (
-    <div className="bg-grey-50 rounded-[10px] flex justify-between items-center w-[157px] h-[56px]">
+    <div className="bg-grey-50 px-3 py-[14px] rounded-[10px] flex justify-between items-center">
       <button type="button" className="cursor-pointer px-2 py-2">
         <img src={iconMinus} alt="Minus icon" />
       </button>
