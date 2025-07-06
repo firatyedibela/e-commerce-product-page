b09d246429480d335e94fdb547fa276f79bae564
firatyedibela <yedibelafirat0@gmail.com>
2025-07-06 03:03:05 +0300

    feat: use svg instead of image for nav arrows

--- src/components/NextButton.tsx
+++ src/components/NextButton.tsx
@@ -2,12 +2,35 @@ import nextIcon from '../assets/images/icon-next.svg';
 
 export const NextButton = () => {
   return (
-    <button className="w-14 h-14 bg-white rounded-full relative cursor-pointer">
-      <img
-        className="absolute top-1/2 right-1/2 translate-x-2/3 -translate-y-1/2"
-        src={nextIcon}
-        alt="Next icon"
-      />
+    <button
+      role="button"
+      className="cursor-pointer absolute top-1/2 right-1/2 -translate-y-1/2 translate-x-1/2"
+    >
+      <svg
+        width="11"
+        height="14"
+        viewBox="0 0 11 14"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <g clip-path="url(#clip0_6102_4)">
+          <path
+            d="M2.5 1.28571L8.21429 6.99999L2.5 12.7143"
+            stroke="#1D2026"
+            stroke-width="3"
+          />
+        </g>
+        <defs>
+          <clipPath id="clip0_6102_4">
+            <rect
+              width="10"
+              height="14"
+              fill="white"
+              transform="translate(0.5)"
+            />
+          </clipPath>
+        </defs>
+      </svg>
     </button>
   );
 };
--- src/components/PrevButton.tsx
+++ src/components/PrevButton.tsx
@@ -2,12 +2,23 @@ import previousIcon from '../assets/images/icon-previous.svg';
 
 export const PrevButton = () => {
   return (
-    <button className="w-14 h-14 bg-white rounded-full relative cursor-pointer">
-      <img
-        className="absolute top-1/2 left-1/2 -translate-x-2/3 -translate-y-1/2"
-        src={previousIcon}
-        alt="Next icon"
-      />
+    <button
+      role="button"
+      className="cursor-pointer absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2"
+    >
+      <svg
+        width="9"
+        height="14"
+        viewBox="0 0 9 14"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M7.85718 1.28571L2.14289 6.99999L7.85718 12.7143"
+          stroke="#1D2026"
+          stroke-width="3"
+        />
+      </svg>
     </button>
   );
 };
