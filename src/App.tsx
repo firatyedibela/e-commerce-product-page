2a0d4af9ddd9dc1d1989e8852c085f25d6310b71
firatyedibela <yedibelafirat0@gmail.com>
2025-07-06 22:05:25 +0300

    feat: render landmark components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { Header } from './components/Header';
+import { Main } from './components/Main';
+
 function App() {
-  return <h1 className="text-orange-500 font-display">Hello world</h1>;
+  return (
+    <div className="max-w-[375px] md:max-w-[608px] xl:max-w-[1110px] m-auto pt-5 md:pt-7 md:gap-12 xl:gap-24 flex flex-col gap-6 font-main">
+      <Header />
+      <Main />
+    </div>
+  );
 }
 
 export default App;
