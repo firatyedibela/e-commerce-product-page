bbfadb015e294e90b46f1f7a3d45be6279f2db2b
firatyedibela <yedibelafirat0@gmail.com>
2025-07-06 19:09:00 +0300

    style: add tablet styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,7 @@ import cart from '../assets/images/icon-cart.svg';
 
 export const Header = () => {
   return (
-    <header className="m-auto px-6 xs:px-0 lg:max-w-full mt-5 mb-6 flex justify-between">
+    <header className="px-6 xs:px-0 lg:max-w-full flex justify-between items-center md:pb-8 md:border-b md:border-grey-100">
       <div className="flex flex-row-reverse lg:flex-row items-start gap-4 justify-end">
         <img src={logo} alt="Sneakers website logo" />
         <nav>
@@ -15,12 +15,16 @@ export const Header = () => {
           <MenuMobile routes={routes} />
         </nav>
       </div>
-      <div className="flex gap-6">
+      <div className="flex gap-6 md:gap-12">
         <button className="cursor-pointer">
           <img src={cart} alt="Shopping cart icon" />
         </button>
         <button className="cursor-pointer">
-          <img className="w-6 h-6" src={avatar} alt="Avatar icon" />
+          <img
+            className="w-6 h-6 md:w-[50px] md:h-[50px]"
+            src={avatar}
+            alt="Avatar icon"
+          />
         </button>
       </div>
     </header>
