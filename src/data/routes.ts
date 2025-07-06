34e0dc00f079d7c787758f59aea1faeaf04c588c
firatyedibela <yedibelafirat0@gmail.com>
2025-07-06 17:44:17 +0300

    feat: add routes

---
+++ src/data/routes.ts
@@ -0,0 +1,24 @@
+import type { TRoutes } from '../types/routes';
+
+export const routes: TRoutes = [
+  {
+    name: 'collections',
+    href: '#',
+  },
+  {
+    name: 'men',
+    href: '#',
+  },
+  {
+    name: 'women',
+    href: '#',
+  },
+  {
+    name: 'about',
+    href: '#',
+  },
+  {
+    name: 'contact',
+    href: '#',
+  },
+];
