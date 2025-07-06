86f1bffc88f52a377d7421b854d3c9016aa0742a
firatyedibela <yedibelafirat0@gmail.com>
2025-07-06 20:46:45 +0300

    refactor: implement and use reusable NavButton component

--- src/components/MobileSwiper.tsx
+++ src/components/MobileSwiper.tsx
@@ -1,7 +1,6 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, A11y } from 'swiper/modules';
-import { NextButton } from './NextButton';
-import { PrevButton } from './PrevButton';
+import { NavButton } from './NavButton';
 import type { TImage } from '../types/product';
 
 type MobileSwiperProps = {
@@ -28,12 +27,8 @@ export const MobileSwiper = ({ images, className }: MobileSwiperProps) => {
             ></img>
           </SwiperSlide>
         ))}
-        <div className="w-10 h-10 bg-white rounded-full prev absolute top-1/2 -translate-y-3/4 left-4 z-5">
-          <PrevButton />
-        </div>
-        <div className="w-10 h-10 bg-white rounded-full next absolute top-1/2 -translate-y-3/4 right-4 z-5">
-          <NextButton />
-        </div>
+        <NavButton direction="prev" />
+        <NavButton direction="next" />
       </Swiper>
     </div>
   );
---
+++ src/components/NavButton.tsx
@@ -0,0 +1,56 @@
+import { clsx } from 'clsx';
+
+type NavButtonProps = {
+  direction: string;
+};
+
+export const NavButton = ({ direction }: NavButtonProps) => {
+  const next = direction === 'next';
+  const baseStyles =
+    'nav-btn w-10 h-10 bg-white rounded-full absolute top-1/2 -translate-y-3/4 z-5 cursor-pointer';
+  const directionalStyles = next ? 'next right-4' : 'prev left-4';
+
+  return (
+    <button className={clsx(baseStyles, directionalStyles)} type="button">
+      <NavSvg next={next} />
+    </button>
+  );
+};
+
+type NavSvgProps = {
+  next: boolean;
+};
+
+const NavSvg = ({ next }: NavSvgProps) => {
+  return next ? (
+    <svg
+      className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2"
+      width="10"
+      height="14"
+      viewBox="0 0 10 14"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M2 1.28571L7.71429 6.99999L2 12.7143"
+        stroke="#1D2026"
+        strokeWidth="3"
+      />
+    </svg>
+  ) : (
+    <svg
+      className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2"
+      width="9"
+      height="14"
+      viewBox="0 0 9 14"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M7.85718 1.28571L2.14289 6.99999L7.85718 12.7143"
+        stroke="#1D2026"
+        strokeWidth="3"
+      />
+    </svg>
+  );
+};
