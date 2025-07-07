c2259b1276ca0cffc7afb78b830a71991826afc9
firatyedibela <yedibelafirat0@gmail.com>
2025-07-07 23:19:30 +0300

    feat: add logic to conditionally render Ligthbox

--- src/components/ImageGallery.tsx
+++ src/components/ImageGallery.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { clsx } from 'clsx';
 import type { TImage } from '../types/product';
 import { Thumbnail } from './Thumbnail';
+import { Lightbox } from './Lightbox';
 
 type ImageGalleryProps = {
   className: string;
@@ -10,18 +11,31 @@ type ImageGalleryProps = {
 
 export const ImageGallery = ({ className, images }: ImageGalleryProps) => {
   const [activeImageIndex, setActiveImageIndex] = useState<number>(0);
+  const [isLightbox, setLightbox] = useState<boolean>(false);
 
   const handleThumbnailClick = (index: number) => {
     setActiveImageIndex(index);
   };
 
+  const handleLightboxClose = () => {
+    setLightbox(false);
+  };
+
   return (
     <div className={clsx(className, 'flex-col gap-8')}>
+      {isLightbox && (
+        <Lightbox
+          images={images}
+          currentImageIndex={activeImageIndex}
+          onClose={handleLightboxClose}
+        />
+      )}
       <div>
         <img
           className="rounded-[15px]"
           src={images[activeImageIndex].original}
           alt=""
+          onClick={() => setLightbox(true)}
         />
       </div>
       <div>
