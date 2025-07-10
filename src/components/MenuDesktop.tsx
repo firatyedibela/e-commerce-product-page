771589a138a340dcf3d77d3e85bb95c0f5ed9fca
firatyedibela <yedibelafirat0@gmail.com>
2025-07-11 02:17:38 +0300

    feat: add active route styles

--- src/components/MenuDesktop.tsx
+++ src/components/MenuDesktop.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import type { TRoutes } from '../types/routes';
 
 type MenuDesktopProps = {
@@ -5,12 +6,25 @@ type MenuDesktopProps = {
 };
 
 export const MenuDesktop = ({ routes }: MenuDesktopProps) => {
+  const [activeRoute, setActiveRoute] = useState<string | null>(null);
+
   return (
     <ul className="hidden xl:flex items-center gap-8 text-preset-4 text-grey-500">
       {routes.map((route) => (
         <li key={route.name}>
-          <a className="capitalize" href={route.href}>
+          <a
+            onClick={() => setActiveRoute(route.name)}
+            className={`${
+              route.name === activeRoute && 'text-grey-950'
+            } capitalize relative hover:text-grey-950`}
+            href={route.href}
+          >
             {route.name}
+            <div
+              className={`${
+                route.name === activeRoute ? 'flex' : 'hidden'
+              } w-full h-[4px] top-16 bg-orange-500 absolute`}
+            ></div>
           </a>
         </li>
       ))}
