dd419254758c91b0fab7e4948115993909525744
firatyedibela <yedibelafirat0@gmail.com>
2025-07-11 01:04:18 +0300

    feat: make Cart appear on top

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,13 @@
 import { MenuDesktop } from './MenuDesktop';
 import { MenuMobile } from './MenuMobile';
+import { Cart } from './Cart';
 import { routes } from '../data/routes';
 import logo from '../assets/images/logo.svg';
 import avatar from '../assets/images/image-avatar.png';
-import cart from '../assets/images/icon-cart.svg';
 
 export const Header = () => {
   return (
-    <header className="px-6 xs:px-0 xl:max-w-full flex justify-between items-center md:pb-8 md:border-b md:border-grey-100">
+    <header className="px-6 xs:px-0 xl:max-w-full flex justify-between items-center md:pb-8 md:border-b md:border-grey-100 relative z-10">
       <div className="flex flex-row-reverse xl:flex-row items-start gap-4 xl:gap-16 justify-end">
         <img className="xl:self-end" src={logo} alt="Sneakers website logo" />
         <nav>
@@ -15,10 +15,8 @@ export const Header = () => {
           <MenuMobile routes={routes} />
         </nav>
       </div>
-      <div className="flex gap-6 md:gap-12">
-        <button className="cursor-pointer">
-          <img src={cart} alt="Shopping cart icon" />
-        </button>
+      <div className="flex gap-6 md:gap-12 items-center md:relative">
+        <Cart />
         <button className="cursor-pointer">
           <img
             className="w-6 h-6 md:w-[50px] md:h-[50px]"
