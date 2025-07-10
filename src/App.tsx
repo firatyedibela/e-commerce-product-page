ebfb9fb0ec48c268aae57f3c936daa88aab75de7
firatyedibela <yedibelafirat0@gmail.com>
2025-07-10 19:41:22 +0300

    feat: wrap App with CartProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { Header } from './components/Header';
 import { Main } from './components/Main';
+import { CartProvider } from './context/CartContext';
 
 function App() {
   return (
-    <div className="max-w-[375px] md:max-w-[608px] xl:max-w-[1110px] m-auto pt-5 md:pt-7 md:gap-12 xl:gap-24 flex flex-col gap-6 font-main">
-      <Header />
-      <Main />
-    </div>
+    <CartProvider>
+      <div className="max-w-[375px] md:max-w-[608px] xl:max-w-[1110px] m-auto pt-5 md:pt-7 md:gap-12 xl:gap-24 flex flex-col gap-6 font-main">
+        <Header />
+        <Main />
+      </div>
+    </CartProvider>
   );
 }
 
