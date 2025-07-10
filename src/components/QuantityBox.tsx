c640c3b6dff0af226ce1bce1af0856581d7c8ccc
firatyedibela <yedibelafirat0@gmail.com>
2025-07-11 01:42:43 +0300

    feat: add aria attributes

--- src/components/QuantityBox.tsx
+++ src/components/QuantityBox.tsx
@@ -16,14 +16,22 @@ export const QuantityBox = ({
     <div className="w-full bg-grey-50 px-3 py-[14px] rounded-[10px] flex justify-between items-center xl:flex-1">
       <button
         type="button"
+        aria-label="Decrease item quantity by 1"
         className="cursor-pointer px-2 py-2"
         onClick={onDecrease}
       >
         <img src={iconMinus} alt="Minus icon" />
       </button>
-      <span className="text-preset-3 text-grey-950">{quantity}</span>
+      <span
+        aria-live="polite"
+        aria-label="Item quantity"
+        className="text-preset-3 text-grey-950"
+      >
+        {quantity}
+      </span>
       <button
         type="button"
+        aria-label="Increase item quantity by 1"
         className="cursor-pointer px-2 py-2"
         onClick={onIncrease}
       >
